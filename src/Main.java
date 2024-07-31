770f5a54e61a4712998c2c2e94a028816b5d1fd1
GvantsaKiknadzee <gvantsa.kiknadze17@gmail.com>
2024-07-31 12:50:44 +0400

    TRANSFER CALCULATOR

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,36 @@
 import transfer.TransferCalculator;
 
+import java.time.LocalDate;
 import java.time.Month;
 import java.util.Map;
-import java.util.Optional;
-
 
 public class Main {
     public static void main(String[] args) {
-        TransferCalculator calculator = new TransferCalculator();
+        double[] transfers = {
+                6057.40095900678, 8836.61529909037, 9764.25444382109, 7497.90805349589,
+                4357.2732070142, 720.012565739051, 6172.98568982675, 3955.22909517134,
+                6139.59444074629, 6789.37297148259, 3784.11360901859, 8038.21668311446,
+                5890.00828639693, 6968.97683238347, 5482.93729511712, 262.009201755319,
+                4106.92706255898, 9971.85217862274, 7207.67382465715, 4488.61857827778
+        };
+
+        LocalDate[] transferDates = {
+                LocalDate.of(1905, 7, 13), LocalDate.of(2023, 10, 15), LocalDate.of(2024, 4, 15), LocalDate.of(2024, 1, 15),
+                LocalDate.of(2024, 7, 13), LocalDate.of(2024, 7, 13), LocalDate.of(2023, 3, 15), LocalDate.of(2023, 2, 15),
+                LocalDate.of(2024, 1, 15), LocalDate.of(2023, 7, 15), LocalDate.of(2023, 7, 15), LocalDate.of(2022, 12, 15),
+                LocalDate.of(2024, 6, 15), LocalDate.of(2024, 7, 13), LocalDate.of(2024, 6, 15), LocalDate.of(2024, 7, 13),
+                LocalDate.of(2024, 3, 15), LocalDate.of(2024, 5, 15), LocalDate.of(2024, 7, 13), LocalDate.of(2024, 7, 13)
+        };
+        TransferCalculator calculator = new TransferCalculator(transfers,transferDates);
+
         System.out.println("Last 6 months average transfer: " + calculator.calculateLast6MonthsAverageTransfer());
 
-        Map<Month, Double> maxMonthlyTransfers = calculator.calculateMaxMonthlyTransfer();
-        printMaxMonthlyTransfers(maxMonthlyTransfers);
-
-        findAndPrintMonthWithMaxAmount(maxMonthlyTransfers);
-    }
-
-    private static void printMaxMonthlyTransfers(Map<Month, Double> maxMonthlyTransfers) {
-        System.out.println("Month        Maximum Transfer Amount");
-        System.out.println("-------------------------------------");
-        for (Map.Entry<Month, Double> entry : maxMonthlyTransfers.entrySet()) {
-            System.out.printf("%-12s : %10.2f%n", entry.getKey(), entry.getValue());
+        Map.Entry<Month, Double> maxMonthlyTransfer = calculator.calculateMaxMonthlyTransfer();
+        if (maxMonthlyTransfer != null) {
+            System.out.println("Highest Month value was in " + maxMonthlyTransfer.getKey()
+                    + " and amount was " + maxMonthlyTransfer.getValue());
+        } else {
+            System.out.println("No valid transfers found.");
         }
-    }
-
-    private static void findAndPrintMonthWithMaxAmount(Map<Month, Double> maxMonthlyTransfers) {
-        Optional<Map.Entry<Month, Double>> monthWithMaxAmountOptional = maxMonthlyTransfers.entrySet().stream()
-                .max(Map.Entry.comparingByValue());
-
-        monthWithMaxAmountOptional.ifPresent(monthWithMaxAmount ->
-                System.out.println("Month with highest amount is " + monthWithMaxAmount.getKey() +
-                        " with amount of " + monthWithMaxAmount.getValue())
-        );
     }
 }
